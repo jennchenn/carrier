05d56c13573f000f4b8cb66affc34acb9eb3ae8e
Yzabelle Go <52092038+yzzy2go@users.noreply.github.com>
2021-08-28 16:56:26 -0400

    Added basic nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Breadcrumbs, Groups, Group, IssueMaker } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Breadcrumbs />
+        <Switch>
+          <Route path="/" exact component={() => <Groups />} />
+          <Route path="/Groups/Group" exact component={() => <Group />} />
+          <Route path="/Groups/Group/IssueMaker" exact component={() => <IssueMaker />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
