dcf94d409a02249799c404da8d706770429198c7
Yzabelle Go <52092038+yzzy2go@users.noreply.github.com>
2021-08-28 21:14:57 -0400

    changes to background colour and group buttons

--- frontend/src/components/IssueMaker.jsx
+++ frontend/src/components/IssueMaker.jsx
@@ -1,27 +1,19 @@
 import React from "react";
 
-import { getPost } from '../api/post';
-
 class IssueMaker extends React.Component {
     constructor(props) {
         super(props);
         this.state = { title: '' }
     }
 
-    onTitleChange = (e, data) => {
+    onQuestionChange = (e, data) => {
         const { value } = data;
         this.setState({
             name: value,
         });
     }
 
-    getPost = async () => {
-        const post = await getPost("612a97ef3ee421234cf2d926");
-        console.log(post);
-    }
-
     onSavePost = event => {
-        this.getPost();
         event.preventDefault();
         alert('You have submitted the form.')
     }
@@ -30,22 +22,26 @@ class IssueMaker extends React.Component {
         return (
             <form onSubmit={this.onSavePost}>
                 <h1>Post Creator</h1>
-                <p>QUESTION</p>
-                <input
-                    type="text"
-                />
-                <p>QUESTION</p>
-                <input
-                    type="text"
-                />
-                <p>QUESTION</p>
-                <input
-                    type="text"
-                />
-                <br></br>
-                <input
-                    type="submit">
-                </input>
+                <div id="wrap">
+                    <div id="content">
+                        <p>QUESTION</p>
+                        <input
+                            type="text"
+                        />
+                        <p>QUESTION</p>
+                        <input
+                            type="text"
+                        />
+                        <p>QUESTION</p>
+                        <input
+                            type="text"
+                        />
+                        <br></br>
+                        <input
+                            type="submit">
+                        </input>
+                    </div>
+                </div>
             </form >
         );
     }
